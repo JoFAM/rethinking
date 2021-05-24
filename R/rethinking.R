7e19087b4cc57ba49bc21ee3d71edac45dca49a1
Joris Meys <Joris.Meys@UGent.be>
2021-05-24 18:40:13 +0200

    Package help page in R (allows for import tags to be centralized once namespace is automated)

---
+++ R/rethinking.R
@@ -0,0 +1,26 @@
+#' Statistical Rethinking package
+#' 
+#' This package accompanies a book and course on Bayesian data
+#' analysis, featured MAP estimation through \code{\link{quap}}
+#' and Hamiltonian Monte Carlo through \code{\link{ulam}}.
+#' 
+#'   \tabular{ll}{
+#' Package: \tab rethinking\cr
+#' Type: \tab Package\cr
+#' License: \tab GPL-3 \cr
+#' }
+#' 
+#' @seealso \code{\link{quap}}, \code{\link{ulam}}
+#' 
+#' @author Richard McElreath
+#' @references McElreath (2020). Statistical Rethinking: A Bayesian Course with Examples in R and Stan (2nd edition). CRC Press.
+#' 
+#' @import stats graphics methods
+#' 
+#' @keywords rethinking package
+#' @rdname rethinking-package
+#' @name rethinking-package
+#' @aliases rethinking
+#' @docType package
+#' 
+"_PACKAGE"
