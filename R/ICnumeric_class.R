db7f6d1926cb9f4827c93451fa8e05909f99574d
Joris Meys <Joris.Meys@UGent.be>
2021-05-24 14:41:39 +0200

    Solves general warnings about non-standard files, non-standard characters and unportable file names in R CMD check

--- R/ICnumeric_class.R
+++ R/ICnumeric_class.R
@@ -5,7 +5,7 @@ setClass( "ICnumeric" , contains="numeric" )
 setMethod( "show" , "ICnumeric" , function(object) {
     cat( round( object , 1 ) )
     if ( !is.null( attr(object,"se") ) ) {
-        cat( concat( " ±" , round( attr(object,"se") , 1 ) ) )
+        cat( concat( " \u00B1 " , round( attr(object,"se") , 1 ) ) )
     }
     cat("\n")
 } )
